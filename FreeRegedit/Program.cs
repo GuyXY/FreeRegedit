13cbd6d3bed9b7de806e3165569964e4e12bfae0
Joel <joel@welti-web.ch>
2015-02-18 19:56:24 +0100

    Initialization
    Initialization

---
+++ FreeRegedit/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeRegedit
+{
+    class Program
+    {
+
+        private static byte counter = 0;
+
+        static void Main(string[] args)
+        {
+            string keyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options";
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(keyPath, true);
+            DeleteValue(OpenSubKey(key, "regedit.exe"));
+            DeleteValue(OpenSubKey(key, "reg.exe"));
+            Console.WriteLine(counter + " entries were removed!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static RegistryKey OpenSubKey(RegistryKey key, string subkey)
+        {
+            return key.OpenSubKey(subkey, true);
+        }
+
+        private static void DeleteValue(RegistryKey key)
+        {
+            if (key != null && key.GetValue("Debugger") != null)
+            {
+                key.DeleteValue("Debugger");
+                counter++;
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# FreeRegedit
+FreeRegedit attempts to give you access to your registry again.
+
+Download: <a href="https://github.com/GuyXY/FreeRegedit/blob/master/FreeRegedit/bin/Release/FreeRegedit.exe?raw=true">here</a>
+
+This project is licensed under the <a href="http://en.wikipedia.org/wiki/WTFPL">WTFPL</a> license.
